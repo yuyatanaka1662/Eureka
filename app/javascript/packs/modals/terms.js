453b3ea12898b719191f4b29e353d1ebdd15308a
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-09-09 01:25:07 +0900

    Fix: 利用規約とプライバシーポシリーのjsファイルを分け、それぞれを読み込み

--- app/javascript/packs/modals/terms.js
+++ app/javascript/packs/modals/terms.js
@@ -10,16 +10,3 @@ document.addEventListener('turbolinks:load', () => {
     })
   });
 });
-
-document.addEventListener('turbolinks:load', () => {
-  $(function() {
-    $('.js-privacy-policy-modal-open').click(function() {
-      $('.js-privacy-policy-modal').fadeIn();
-      return false;
-    });
-    $('.js-privacy-policy-modal-close').click(function() {
-      $('.js-privacy-policy-modal').fadeOut();
-      return false;
-    })
-  });
-});
