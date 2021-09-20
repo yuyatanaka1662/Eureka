9bd7488da5130d6d6246737ebc13647aa0feb89d
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-09-20 18:47:48 +0900

    Add:jsファイル作成

---
+++ app/javascript/packs/questions/question1.js
@@ -0,0 +1,8 @@
+// document.addEventListener('turbolinks:load', () => {
+//   $(function() {
+//     $('#button_a, #button_b').hover(
+//       function() {
+//         $('#question_title').html('<h4>aaa</h4>');
+//     });
+//   });
+// });
