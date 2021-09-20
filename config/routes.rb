ca797614252548d0ff610f41f0b0651f52b5ad26
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-09-20 18:51:13 +0900

    Fix:ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root 'home#top'
   # resources :questions
-  get 'shindan', to: 'questions#new', as: 'shindan'
+  get 'shindan1', to: 'questions#question1', as: 'shindan1'
+  get 'shindan2', to: 'questions#question2', as: 'shindan2'
+  get 'shindan3', to: 'questions#question3', as: 'shindan3'
+  get 'shindan4', to: 'questions#question4', as: 'shindan4'
+  get 'shindan5', to: 'questions#question5', as: 'shindan5'
+  # post 'shindan1', to: 'questions#create'
+  post 'shindan2', to: 'questions#create'
+  put 'shindan3', to: 'questions#update'
+  put 'shindan4', to: 'questions#update'
+  put 'shindan5', to: 'questions#update'
+  # put 'shindan3', to: 'questions#create'
+  put 'results', to: 'questions#update'
+  post 'results', to: 'questions#create'
+  resources :results, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
