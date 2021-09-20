f7f201576850d81ce6337f3226cbd5f10b5e0454
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-09-20 18:51:53 +0900

    Fix:コントローラ修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,56 @@
 class QuestionsController < ApplicationController
-  def new
-    @tag = Tag.new
+  def create
+    # tag = []
+    # binding.pry
+    # @tag = tag << params[:tag][:name]
+    # binding.pry
+    answers = []
+    # binding.pry
+    @answers = answers << params[:tag][:name]
+    # binding.pry
+    redirect_to shindan2_path
+    # binding.pry
+    # @islands = Island.all
+    # @islands.joins(:tags).each do |island|
+    #   @tag_name = island.tags.pluck(:name)
+    #   binding.pry
+    #   if @tag_name == @answers
+    #     binding.pry
+    #     redirect_to shindan2_path
+    #   end
+    # end
+
+    # Island.joins(:tags).select('tags.name').each do |tag|
+    #   # binding.pry
+    #   if @answers == tag.name.split('')
+    #     redirect_to results_path
+    #   end
+    # end
   end
 
-  def create
-    @tag = Tag.create(tag_params)
+  def update
+    # get_answers
+    # binding.pry
+    answers = []
+    # binding.pry
+    @answers = answers << params[:tag][:name]
+    # binding.pry
+    # answers << @tag
+    # binding.pry
+    redirect_to shindan3_path
   end
 
   private
 
-  def tag_params
-    params.requier(:tag).permit(:name)
-  end
+  # def get_answers
+  #   @answers = [] << params[:tag][:name]
+  # end
+
+  # def tag_params
+  #   params.require(:tag).permit(:name)
+  # end
+
+  # def get_tag
+  #   @tag = params[:tag][:name]
+  # end
 end
