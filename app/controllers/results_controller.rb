2c9d0793c85e48e7eac4912a643b6cf2b7081118
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-11-06 17:16:04 +0900

    fix:結果表示アクション修正

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,9 @@
 class ResultsController < ApplicationController
   def index
     # binding.pry
+    session[:answer] << params[:tag][:name]
+    # binding.pry
     @islands = Island.all
+    # binding.pry
   end
 end
