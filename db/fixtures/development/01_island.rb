f1cc4d0d8b229931777a92b3429a33b2399fe01f
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-11-15 12:57:31 +0900

    add: 初期データ追加

--- db/fixtures/development/01_island.rb
+++ db/fixtures/development/01_island.rb
@@ -1,74 +1,91 @@
 Island.seed(:id,
   { id: 1,
     name: '友ヶ島',
-    description: '和歌山県にある無人島「友ヶ島」。某天空の城の迷い込んだような気分を味わえると人気の島です。砲台跡や弾薬庫跡など、旧日本軍の施設跡が点在していてハイキングがてらそれらを巡るのが定番コースです。',
-    information: 'http://www.city.wakayama.wakayama.jp/kankou/kankouspot/1027585/1027593.html(和歌山市ホームページ)',
+    description: '和歌山県にある無人島「友ヶ島」。某天空の城の迷い込んだような気分を味わえると人気の島です。
+                  砲台跡や弾薬庫跡など、旧日本軍の施設跡が点在していてハイキングがてらそれらを巡るのが定番コースです。',
+    information: 'http://www.city.wakayama.wakayama.jp/kankou/kankouspot/1027585/1027593.html',
     address: '〒640-0102 和歌山県和歌山市深山',
     latitude: 34.28217309472821,
     longitude: 135.01270774795321 },
+
   { id: 2,
     name: '屋久島',
-    description: '屋久島の説明',
-    information: '屋久島のinfo',
-    address: '屋久島の住所',
-    latitude: 0.1,
-    longitude: 0.2 },
+    description: '樹齢3000年以上とも言われる縄文杉が見られる、鹿児島県にある「屋久島」。
+                  トレッキングはもちろんのこと、永田浜は日本一のウミガメの産卵地として有名で、
+                  アカウミガメが産卵のために上陸する毎年5月から7月にかけて、「ウミガメ観察会」が開催されています。
+                  大自然や生命の神秘にどっぷり浸かりにいきましょう。',
+    information: 'http://yakukan.jp/',
+    address: '鹿児島県熊毛郡',
+    latitude: 30.369490703058574,
+    longitude: 130.53118928396322 },
+
   { id: 3,
     name: '池島',
-    description: '長崎県にある、第二の軍艦島とも呼ばれる「池島」。かつて炭鉱で栄えましたが現在の人口は130名ほど。炭鉱跡や居住区跡などが残っており、廃墟マニアにはたまりません。',
-    information: 'https://www.city.nagasaki.lg.jp/shimin/121000/121200/p027560.html(長崎市ホームページ)',
+    description: '長崎県にある、第二の軍艦島とも呼ばれる「池島」。
+                  かつて炭鉱で栄えましたが現在の人口は130名ほど。
+                  炭鉱跡や居住区跡などが残っており、廃墟マニアにはたまりません。',
+    information: 'https://www.city.nagasaki.lg.jp/shimin/121000/121200/p027560.html',
     address: '〒857-0071 長崎県長崎市池島町',
     latitude: 32.885265759271725,
     longitude: 129.59892805747407 },
+
   { id: 4,
     name: '犬島',
-    description: '犬島の説明',
-    information: '犬島のinfo',
-    address: '犬島の住所',
-    latitude: 0.3,
-    longitude: 0.4 },
-  {
-    id: 5,
+    description: "瀬戸内海に浮かぶ、岡山県の「犬島」。
+                  かつて栄えた銅の精錬所跡が現在は美術館に生まれ変わり、外を歩けば廃工場のノスタルジーを感じ、中に入れば現代アートに触れることができます。
+                  「家プロジェクト」と称し、集落にギャラリーが点在しており、島内を歩くだけでも様々な作品を見ることができます。",
+    information: 'https://www.okayama-kanko.jp/spot/10162',
+    address: '〒704-8153 岡山県岡山市東区犬島',
+    latitude: 34.5634974718975,
+    longitude: 134.10203146631451 },
+
+  { id: 5,
     name: '沖島',
-    description: '沖島の説明',
-    information: '沖島のinfo',
-    address: '滋賀県',
-    latitude: 0.5,
-    longitude: 0.6 },
-  {
-    id: 6,
+    description: '滋賀県、琵琶湖に浮かぶ、世界でも珍しい湖沼の有人島「沖島」。
+                  漁に従事する島民が多く、湖水の島らしく鮎や鮒などの淡水魚を頂くことができます。
+                  また、日本人ならどこか懐かしく感じてしまう街並みが続き、童心にかえって散歩するのもいいかもしれません。',
+    information: 'https://www.biwako-visitors.jp/',
+    address: '〒523-0801 滋賀県近江八幡市沖島町',
+    latitude: 35.20784130851707,
+    longitude: 136.06386906305482 },
+
+  { id: 6,
     name: '柏島',
-    description: '柏島の説明',
-    information: '柏島のinfo',
-    address: '高知県',
-    latitude: 0.7,
-    longitude: 0.8 },
-  {
-    id: 7,
+    description: '透き通るほどキレイな海が広がる高知県「柏島」。
+                  エメラルドグリーンの海に浮かんだ船がまるで宙に浮いているよう、とSNSで話題にもなりました。
+                  ダイビング施設や高台にあるキャンプ場など自然を満喫するのに絶好の島です。',
+    information: 'https://www.town.otsuki.kochi.jp/kanko/info/kashiwajima.php',
+    address: '〒788-0343 高知県幡多郡大月町柏島',
+    latitude: 32.7695873708701,
+    longitude: 132.62317229019834 },
+
+  { id: 7,
     name: '大久野島',
-    description: '大久野島の説明',
-    information: '大久野島のinfo',
-    address: '広島県',
-    latitude: 0.9,
-    longitude: 1.0 },
-  {
-    id: 8,
+    description: '「うさぎ島」としても知られ900羽以上のうさぎ達が暮らす、瀬戸内海に浮かぶ広島県「大久野島」。
+                  ほのぼのする印象とは裏腹に、1929年から1945年に太平洋戦争が終わるまで、日本軍が毒ガスの製造を行っており、地図上から消されていたという歴史があります。
+                  毒ガス貯蔵庫跡や資料館など、凄惨な歴史を今に伝える施設が点在し、それらを巡ることで平和について考えるキッカケになるかもしれません。',
+    information: 'https://www.qkamura.or.jp/ohkuno/',
+    address: '〒729-2311 広島県竹原市忠海町',
+    latitude: 34.30940235311755,
+    longitude: 132.99403629386188 },
+
+  { id: 8,
     name: '竹富島',
     description: '竹富島の説明',
     information: '竹富島のinfo',
     address: '沖縄県',
     latitude: 1.1,
     longitude: 1.2 },
-  {
-    id: 9,
+
+  { id: 9,
     name: '青ヶ島',
     description: '青ヶ島の説明',
     information: '青ヶ島のinfo',
     address: '東京都',
     latitude: 1.3,
     longitude: 1.4 },
-  {
-    id: 10,
+
+  { id: 10,
     name: '軍艦島(端島)',
     description: '軍艦島の説明',
     information: '軍艦島のinfo',
