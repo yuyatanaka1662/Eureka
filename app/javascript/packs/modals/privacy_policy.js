3da25b4245381ecdb8d8b07bc92200b84b93838f
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-12-18 18:11:06 +0900

    fix: モーダル内のリンクがうまく作動しなかったため修正

--- app/javascript/packs/modals/privacy_policy.js
+++ app/javascript/packs/modals/privacy_policy.js
@@ -2,11 +2,9 @@ document.addEventListener('turbolinks:load', () => {
   $(function() {
     $('.js-privacy-policy-modal-open').click(function() {
       $('.js-privacy-policy-modal').fadeIn();
-      return false;
     });
     $('.js-privacy-policy-modal-close').click(function() {
       $('.js-privacy-policy-modal').fadeOut();
-      return false;
     })
   });
 });
