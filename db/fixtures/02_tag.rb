e949342ec311b789868d7e26a62f9ea3ae6e1fe8
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2021-12-31 04:22:29 +0900

    fix: タグ名変更

--- db/fixtures/02_tag.rb
+++ db/fixtures/02_tag.rb
@@ -4,9 +4,9 @@ Tag.seed(:id,
   { id: 3, name: '廃墟' },
   { id: 4, name: '芸術' },
   { id: 5, name: 'のんびり' },
-  { id: 6, name: 'アクティビティ' },
-  { id: 7, name: '玄人' },
-  { id: 8, name: '初心者' },
+  { id: 6, name: 'アクティブ' },
+  { id: 7, name: '大きい島' },
+  { id: 8, name: '狭い島' },
   { id: 9, name: '動物' },
-  { id: 10, name: '歴史' }
+  { id: 10, name: '歴史伝統' }
   )
