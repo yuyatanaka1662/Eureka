b0c337ea725b0c72859a1d29b4483ffad782bc45
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2022-01-02 19:11:53 +0900

    add: twitterシェア機能追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import './questions/questions'
 import './results/results'
 import './modals/terms'
 import './modals/privacy_policy'
+import '@fortawesome/fontawesome-free/js/all'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
