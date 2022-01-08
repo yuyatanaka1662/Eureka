1f9764f4fabbba0d8ac80125a023b6840fa10002
yuyatanaka1662 <sankaqu_ba@yahoo.co.jp>
2022-01-08 23:12:54 +0900

    fix: エラーメッセージ表示、リファクタリング

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,10 @@
 class ResultsController < ApplicationController
+  before_action :add_answer_to_session
+
   def results
-    add_answer_to_session
+    if session[:answer].length != 5
+      redirect_to root_path
+    end
     @shuffled_islands = Island.all.shuffle
   end
 end
